0f686d1c44f9fdfa93bd2db24f9d392af12e6221
aviewis <elevatewomeninstem@gmail.com>
2024-10-12 15:50:49 -0700

    Clean code

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,6 @@ import { ConsolePage } from './pages/ConsolePage';
 import React, { useState } from "react"; // TGB
 import './App.scss';
 
-function ToggleButton() {
-  // Define the state that will control the toggle state
-  const [isToggled, setIsToggled] = useState(false);
-
-  // Function to toggle the state
-  const handleToggle = () => {
-    setIsToggled(!isToggled);
-  };
-
-  return (
-    <div>
-      {/* Button changes appearance based on the toggle state */}
-      <button onClick={handleToggle} style={isToggled ? styles.toggled : styles.untoggled}>
-        {isToggled ? "UPDATE UI" : "RESTORE UI"}
-      </button>
-
-      {/* Conditionally render the component based on the toggle state */}
-      {isToggled && <DisplayedComponent />}
-    </div>
-  );
-}
-
 // Define styles for the button when toggled or untoggled
 const styles = {
   toggled: {
@@ -44,18 +22,24 @@ const styles = {
   },
 };
 
-function DisplayedComponent() {
-  return (
-    <ConsolePage />
-  );
-}
-
 function App() {
+  // Define the state that will control the toggle state
+  const [isToggled, setIsToggled] = useState(false);
+
+  // Function to toggle the state
+  const handleToggle = () => {
+    setIsToggled(!isToggled);
+  };
+
   return (
-    <div data-component="App">
-      {/* <ConsolePage /> */}
-      {/* <DisplayedComponent/> */}
-      <ToggleButton/>
+    <div>
+      {/* Button changes appearance based on the toggle state */}
+      <button onClick={handleToggle} style={isToggled ? styles.toggled : styles.untoggled}>
+        {isToggled ? "UPDATE UI" : "RESTORE UI"}
+      </button>
+
+      {/* Conditionally render the component based on the toggle state */}
+      {isToggled && <ConsolePage />}
     </div>
   );
 }
